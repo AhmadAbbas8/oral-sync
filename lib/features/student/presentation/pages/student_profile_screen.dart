efaf0173b9a9a739d06fb2ea315679cf529b4d88
thisismostafahassan <iammostafahassan@gmail.com>
2024-02-16 18:19:45 +0200

    update profile screen

--- lib/features/student/presentation/pages/student_profile_screen.dart
+++ lib/features/student/presentation/pages/student_profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oralsync/features/student/presentation/bloc/student_bloc.dart';
 
 class StudentProfileScreen extends StatelessWidget {
   static const String routeName = '/StudentProfileScreen';
@@ -6,6 +8,95 @@ class StudentProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => StudentBloc(),
+      child: BlocBuilder<StudentBloc, StudentState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text("My Profile"),
+              actions: [
+                TextButton(
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Text("Edit"),
+                    ))
+              ],
+            ),
+            body: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                        'assets/student/images/IMG-20180709-WA0006.jpg'),
+                    maxRadius: 50,
+                  ),
+                  Text(
+                    "Mostafa Hassan",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //
+                  Text(
+                    "----- mostafahassan@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- 01234567891011",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- Male",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- Student at Al-Azahr University",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- Very Good",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- Fourth Academic Year",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "----- Egypt, Cairo, Nasr City",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
