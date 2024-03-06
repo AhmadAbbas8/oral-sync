2c2db94b8fa5d9ce1709656a0011f8a07013774b
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-03-06 12:06:24 +0200

    - solve google font issue - add like and comment to post model - display like counts and post counts

---
+++ lib/features/home_student_feature/data/models/comment_model.dart
@@ -0,0 +1,57 @@
+class CommentModel {
+  CommentModel({
+    this.commentId,
+    this.content,
+    this.title,
+    this.dateCreated,
+    this.timeCreated,
+    this.dateUpdated,
+    this.timeUpdated,
+    this.userId,
+    this.postId,
+    this.user,
+    this.post,
+  });
+
+  CommentModel.fromJson(dynamic json) {
+    commentId = json['commentId'];
+    content = json['content'];
+    title = json['title'];
+    dateCreated = json['dateCreated'];
+    timeCreated = json['timeCreated'];
+    dateUpdated = json['dateUpdated'];
+    timeUpdated = json['timeUpdated'];
+    userId = json['userId'];
+    postId = json['postId'];
+    user = json['user'];
+    post = json['post'];
+  }
+
+  num? commentId;
+  String? content;
+  String? title;
+  String? dateCreated;
+  String? timeCreated;
+  String? dateUpdated;
+  String? timeUpdated;
+  String? userId;
+  num? postId;
+  dynamic user;
+  dynamic post;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['commentId'] = commentId;
+    map['content'] = content;
+    map['title'] = title;
+    map['dateCreated'] = dateCreated;
+    map['timeCreated'] = timeCreated;
+    map['dateUpdated'] = dateUpdated;
+    map['timeUpdated'] = timeUpdated;
+    map['userId'] = userId;
+    map['postId'] = postId;
+    map['user'] = user;
+    map['post'] = post;
+    return map;
+  }
+}
