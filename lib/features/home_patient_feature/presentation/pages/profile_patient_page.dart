f478aa8fb16e03a858594bbd9e497aea2972bd01
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-04-07 18:04:07 +0200

    display patient info

--- lib/features/home_patient_feature/presentation/pages/profile_patient_page.dart
+++ lib/features/home_patient_feature/presentation/pages/profile_patient_page.dart
@@ -4,18 +4,22 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:oralsync/core/helpers/check_language.dart';
 import 'package:oralsync/core/helpers/custom_progress_indicator.dart';
 import 'package:oralsync/core/helpers/extensions/navigation_extensions.dart';
 import 'package:oralsync/core/helpers/snackbars.dart';
 import 'package:oralsync/core/service_locator/service_locator.dart';
 import 'package:oralsync/core/utils/colors_palette.dart';
+import 'package:oralsync/features/Auth/data/models/user_model.dart';
 import 'package:oralsync/features/home_patient_feature/presentation/manager/patient_profile_cubit/patient_profile_cubit.dart';
 
+import '../../../../core/utils/icon_broken.dart';
 import '../../../../core/widgets/circle_avatar.dart';
 import '../../../../translations/locale_keys.g.dart';
 import '../../../home_student_feature/presentation/pages/edit_profile_page.dart';
 import '../../../home_student_feature/presentation/widgets/custom_app_bar_action_button.dart';
+import '../../../../core/widgets/info_widget.dart';
 
 class ProfilePatientPage extends StatelessWidget {
   const ProfilePatientPage({super.key});
@@ -25,16 +29,7 @@ class ProfilePatientPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(LocaleKeys.profile).tr(),
-        actions: [
-          CustomAppBarActionButton(
-            title: LocaleKeys.edit,
-            onTap: () => log('messagemessage'),
-          ),
-          SizedBox(width: 5.w)
-        ],
-      ),
+      appBar: _buildAppBar(),
       body: SingleChildScrollView(
         child: BlocProvider(
           create: (context) => PatientProfileCubit(
@@ -47,12 +42,45 @@ class ProfilePatientPage extends StatelessWidget {
             },
             builder: (context, state) {
               var cubit = context.read<PatientProfileCubit>();
+              var userData = cubit.getUserModel().userDetails as PatientDetails;
               return Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ImageProfileWidget(
                     onPressed: () async => await cubit.changeProfileImage(),
-                    imageProfile: cubit.getUserModel().profileImage ?? '',
+                    imageProfile: state is ChangePatientProfileSuccess
+                        ? state.image
+                        : cubit.getUserModel().profileImage ?? '',
+                  ),
+                  InfoWidget(
+                    title:
+                        '${userData.firstName ?? ''} ${userData.lastName ?? ''}',
+                    icon: IconBroken.User,
+                  ),
+                  InfoWidget(
+                    title: userData.email ?? '',
+                    icon: IconBroken.Message,
+                  ),
+                  InfoWidget(
+                    title: userData.phoneNumber ?? '',
+                    icon: IconBroken.Call,
+                  ),
+                  InfoWidget(
+                    title: userData.isMale ?? true ? 'Male' : 'Female',
+                    icon: userData.isMale ?? true
+                        ? FontAwesomeIcons.male
+                        : FontAwesomeIcons.female,
+                  ),
+                  InfoWidget(
+                    title: userData.birthDate ?? '',
+                    icon: IconBroken.Calendar,
+                  ),
+                  InfoWidget(
+                    title: userData.address
+                        .toString()
+                        .replaceAll(']', '')
+                        .replaceAll('[', ''),
+                    icon: IconBroken.Home,
                   ),
                 ],
               );
@@ -60,6 +88,19 @@ class ProfilePatientPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text(LocaleKeys.profile).tr(),
+      actions: [
+        CustomAppBarActionButton(
+          title: LocaleKeys.edit,
+          onTap: () => log('messagemessage'),
+        ),
+        SizedBox(width: 5.w)
+      ],
     );
   }
 
--- lib/features/home_student_feature/presentation/pages/profile_student_page.dart
+++ lib/features/home_student_feature/presentation/pages/profile_student_page.dart
@@ -13,7 +13,7 @@ import 'package:oralsync/core/utils/icon_broken.dart';
 import 'package:oralsync/core/widgets/custom_rating_bar_widget.dart';
 import 'package:oralsync/features/Auth/data/models/user_model.dart';
 import 'package:oralsync/features/home_student_feature/presentation/widgets/custom_app_bar_action_button.dart';
-import 'package:oralsync/features/home_student_feature/presentation/widgets/info_widget.dart';
+import 'package:oralsync/core/widgets/info_widget.dart';
 import 'package:oralsync/translations/locale_keys.g.dart';
 
 import '../../../../core/helpers/check_language.dart';
