0f1785d57236ce28b1607fe915a0190d817f24ef
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-06-04 16:22:30 +0300

    finish home layout for doctor

--- lib/features/home_doctor_feature/presentation/manager/doctor_cubit/doctor_state.dart
+++ lib/features/home_doctor_feature/presentation/manager/doctor_cubit/doctor_state.dart
@@ -2,9 +2,17 @@ part of 'doctor_cubit.dart';
 
 sealed class DoctorState extends Equatable {
   const DoctorState();
-}
 
-final class DoctorInitial extends DoctorState {
   @override
   List<Object> get props => [];
+}
+
+final class DoctorInitial extends DoctorState {}
+final class BottomNavBarChanged extends DoctorState {
+  final int index;
+
+  const BottomNavBarChanged({required this.index});
+  @override
+  // TODO: implement props
+  List<Object> get props => [index];
 }
--- lib/features/home_doctor_feature/presentation/pages/doctor_home_layout.dart
+++ lib/features/home_doctor_feature/presentation/pages/doctor_home_layout.dart
@@ -1,9 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_lazy_indexed_stack/flutter_lazy_indexed_stack.dart';
 import 'package:oralsync/core/utils/icon_broken.dart';
 import 'package:oralsync/core/widgets/custom_app_drawer.dart';
 import 'package:oralsync/features/home_doctor_feature/presentation/manager/doctor_cubit/doctor_cubit.dart';
+import 'package:oralsync/features/home_doctor_feature/presentation/pages/doctor_home_page.dart';
+import 'package:oralsync/features/home_doctor_feature/presentation/pages/doctor_messages_page.dart';
 
 import '../../../../translations/locale_keys.g.dart';
 
@@ -16,40 +19,49 @@ class HomeDoctorLayoutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => DoctorCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(LocaleKeys.home).tr(),
-        ),
-        drawer: BlocBuilder<DoctorCubit, DoctorState>(
-          builder: (context, state) {
-            var cubit = context.read<DoctorCubit>();
-            return CustomAppDrawer(
+      child: BlocBuilder<DoctorCubit, DoctorState>(
+        builder: (context, state) {
+          var cubit = context.read<DoctorCubit>();
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(LocaleKeys.home).tr(),
+            ),
+            drawer: CustomAppDrawer(
               name:
                   '${cubit.doctorModel.userDetails?.firstName} ${cubit.doctorModel.userDetails?.lastName}',
               email: cubit.doctorModel.userDetails?.email ?? '',
               profileImage: cubit.doctorModel.profileImage ?? '',
-            );
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(IconBroken.Home),
-              tooltip: LocaleKeys.home.tr(),
-              label: LocaleKeys.home.tr(),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(IconBroken.Message),
-              tooltip: LocaleKeys.messages.tr(),
-              label: LocaleKeys.messages.tr(),
+            body: LazyIndexedStack(
+              index: cubit.currentIndex,
+              children: const [
+                HomeDoctorPage(),
+                DoctorMessagesPage(),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(IconBroken.Profile),
-              tooltip: LocaleKeys.profile.tr(),
-              label: LocaleKeys.profile.tr(),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (value) => cubit.onChangeBottomNav(context, value),
+              currentIndex: cubit.currentIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: const Icon(IconBroken.Home),
+                  tooltip: LocaleKeys.home.tr(),
+                  label: LocaleKeys.home.tr(),
+                ),
+                BottomNavigationBarItem(
+                  icon: const Icon(IconBroken.Message),
+                  tooltip: LocaleKeys.messages.tr(),
+                  label: LocaleKeys.messages.tr(),
+                ),
+                BottomNavigationBarItem(
+                  icon: const Icon(IconBroken.Profile),
+                  tooltip: LocaleKeys.profile.tr(),
+                  label: LocaleKeys.profile.tr(),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
